213ad81a7ee35c0e23cea769d81295c62342b351
756650-eng <756650@student.dusd.net>
2026-02-06 11:45:45 -0800

    Log #3
    just added more features

--- P4PlayerControlIbarra/Assets/Course Library/Scripts/PlayerControler.cs
+++ P4PlayerControlIbarra/Assets/Course Library/Scripts/PlayerControler.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
+    private float speed = 23.0f;
+    private float turnSpeed = 10.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,8 +17,10 @@ public class PlayerControler : MonoBehaviour
     {
         // Move the vehicle forward
         horizontalInput = Input.GetAxis("Horizontal");
-
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
