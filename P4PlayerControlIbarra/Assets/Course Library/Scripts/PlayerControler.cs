c0f7f869927ae6020805b32f859d7ee46f25bec3
756650-eng <756650@student.dusd.net>
2026-02-10 11:44:10 -0800

    Log#4
    Added pryamids, oncoming traffic and made camera switcher

--- P4PlayerControlIbarra/Assets/Course Library/Scripts/PlayerControler.cs
+++ P4PlayerControlIbarra/Assets/Course Library/Scripts/PlayerControler.cs
@@ -6,6 +6,9 @@ public class PlayerControler : MonoBehaviour
     private float turnSpeed = 10.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,9 +21,17 @@ public class PlayerControler : MonoBehaviour
         // Move the vehicle forward
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
+
         // Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
         //Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
